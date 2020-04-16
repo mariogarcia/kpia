fee9dc831a6db05999c685584f7e76ec07ee156e
Mario Garcia <mario.ggar@gmail.com>
2020-04-17 01:56:22 +0200

    chores(db): adding golang database code wip

--- projects/services.go
+++ projects/services.go
@@ -12,20 +12,40 @@ type Service struct {
 
 // ListProjects lists all Piweek projects of all times
 func (service *Service) ListProjects(pagination utils.Pagination) utils.PagedResult {
+	results := []Project{}
+	rows, _ := service.DB.Query(`SELECT * FROM projects`)
+
+	// closing connection when logic has finished
+	defer rows.Close()
+
+	// looping results
+	for rows.Next() {
+		pro := Project{}
+		rows.Scan(&pro.ID, &pro.Name, &pro.Description)
+
+		results = append(results, pro)
+	}
+
 	return utils.PagedResult{}
 }
 
 // FindAllMembersByProjectID list all members of a given project
 func (service *Service) FindAllMembersByProjectID(projectID string) utils.PagedResult {
+	rows, err := service.DB.Query(`SELECT * FROM projects_members WHERE project_id = $1`, projectID)
+
 	return utils.PagedResult{}
 }
 
 // FindProjectByID is
 func (service *Service) FindProjectByID(projectID string) Project {
+	row, err := service.DB.QueryRow(`SELECT * FROM project WHERE id = $1`, projectID)
+
 	return Project{}
 }
 
 // FindProjectMemberByID is
 func (service *Service) FindProjectMemberByID(projectID, memberID string) ProjectMember {
+	row, err := service.DB.QueryRow(`SELECT * FROM project_member WHERE project = $1 AND member = $2`, projectID, memberID)
+
 	return ProjectMember{}
 }
